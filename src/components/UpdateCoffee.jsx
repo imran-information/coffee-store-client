8d7139ec3d6fae7824c04f66cb9cd36a967de976
imranhossen21 <imranhossen0586@gmail.com>
2024-11-27 22:02:28 +0600

    updeted

--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,15 +1,104 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const UpdateCoffee = () => {
     const coffee = useLoaderData()
-
     const { _id, name, chef, supplier, taste, category, details, photo } = coffee;
+    const handleCoffeeUpdate = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
 
+        const UpdatedCoffee = { name, chef, supplier, taste, category, details, photo }
+
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(UpdatedCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        title: 'Coffee added!',
+                        text: 'Do you want to continue',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    })
+                }
+            })
+
+    }
 
     return (
         <div>
-            Update Coffee : {name}
+            <h3 className="text-6xl text-center py-10">Update Coffee</h3>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+
+                    <div className="card bg-base-100 w-[600px] shrink-0 shadow-2xl">
+                        <form onSubmit={handleCoffeeUpdate} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Coffee Name</span>
+                                </label>
+                                <input type="text" name="name" defaultValue={name} placeholder="Enter coffee name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Chef</span>
+                                </label>
+                                <input type="text" name="chef" defaultValue={chef} placeholder="Enter coffee chef" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Supplier</span>
+                                </label>
+                                <input type="text" name="supplier" defaultValue={supplier} placeholder="Enter coffee supplier" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Taste</span>
+                                </label>
+                                <input type="text" name="taste" defaultValue={taste} placeholder="Enter coffee taste" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Category </span>
+                                </label>
+                                <input type="text" name="category" defaultValue={category} placeholder="Enter coffee category" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Details</span>
+                                </label>
+                                <input type="number" name="details" defaultValue={details} placeholder="Enter coffee details" className="input input-bordered" required />
+
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo </span>
+                                </label>
+                                <input type="text" name="photo" defaultValue={photo} placeholder="Enter photo URL" className="input input-bordered" required />
+
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Update Coffee</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
